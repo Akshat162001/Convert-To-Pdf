a76e80cedcab0866fbd2b2f8441375c68c0a8071
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2025-01-20 17:14:53 +0530

    Initial Uploads

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="item-center justify-center bg-gray-900">
+        <hr className=" border-black" />
+        <h1 className="text-center py-3 text-lg text-white">
+          Copyright © 2025 - All right reserved by ABC Industries Ltd
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
